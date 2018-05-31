b81a255a44f66c021122aeeaa2e745115a1ea2a1
leecocky <leecocky@hotmail.com>
2018-05-31 12:33:31 +0200

    ajax js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//testing
+function loadDoc() { 
+
+	var xhttp = new XMLHttpRequest(); 
+
+	xhttp.onreadystatechange = function() { 
+
+	if (this.readyState == 4 && this.status == 200) {
+    	var emp = JSON.parse(xhttp.responseText);
+    
+    	for(var i=0;i<emp.length;i++){
+     	let testing =emp[i].name
+      	document.getElementById("demo").innerHTML += testing;  
+
+      }
+	}
+
+}; 
+
+xhttp.open("GET", "test.json", true);  
+xhttp.send();
+
+}
+
+
+loadDoc();
+
